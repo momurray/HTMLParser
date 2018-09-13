01ed7e74dc014517ade1c586b5f1fc7abf8d54ea
Mark Murray <m.murray@pobox.com>
2018-09-13 09:38:43 +0100

    Update....

--- HTMLParse/Program.cs
+++ HTMLParse/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
 
 namespace HTMLParse
 {
@@ -6,7 +8,54 @@ namespace HTMLParse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Make sure there is actually a URL to find images for. 
+            if (args.Length > 0)
+            {
+                GetHtml(args[0]);
+
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Usage: HTMLParse <url> e.g. HTMLParse http://news.bbc.co.uk");
+            }
+
+
         }
+
+        static async void GetHtml(string url)
+        {
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                var result = await client.GetAsync(url);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var content = await result.Content.ReadAsStringAsync();
+
+                    HtmlDocument htmldoc = new HtmlDocument();
+                    htmldoc.LoadHtml(content);
+                    var images = htmldoc.DocumentNode.SelectNodes("//img");
+
+                    foreach (var image in images)
+                    {
+                        if (image.Attributes["src"] != null)
+                        {
+                            Console.WriteLine(image.Attributes["src"].Value);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unable to retreive HTML");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+         }
     }
 }
