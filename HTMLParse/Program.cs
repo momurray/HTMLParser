b0e31a075b2fd2f0dcaa5bd8e9a9adf471d36eb5
Mark Murray <m.murray@pobox.com>
2018-09-13 08:07:16 +0100

    Add project files.

---
+++ HTMLParse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HTMLParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
